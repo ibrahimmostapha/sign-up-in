9c9f2a4ad4ece3f7f54a6d8f65bfa84d5b3a0c60
ibrahimmostapha <ibrahimmoutapha2000@gmail.com>
2023-04-15 22:42:52 +0300

    update db url

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // connect database
-mongoose.connect('mongodb://localhost:27017/mernAuth', {
+mongoose.connect('mongodb://127.0.0.1:27017/mernAuth', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
